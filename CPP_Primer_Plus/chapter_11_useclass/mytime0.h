8faa76ef2857136677afd240bed083dd2af08312
kingstar718 <kingstar718@foxmail.com>
2019-10-31 11:50:53 +0800

    chapter 11

---
+++ CPP_Primer_Plus/chapter_11_useclass/mytime0.h
@@ -0,0 +1,29 @@
+//
+// Created by wujinxing on 2019/10/31.
+//
+
+#ifndef CPLUS_LEARNING_MYTIME0_H
+#define CPLUS_LEARNING_MYTIME0_H
+
+class Time {
+
+private:
+    int hours;
+    int minutes;
+public:
+    Time();
+
+    Time(int h, int m = 0);
+
+    void AddMin(int m);
+
+    void AddHr(int h);
+
+    void Reset(int h = 0, int m = 0);
+
+    Time Sum(const Time &t) const;
+
+    void Show() const;
+};
+
+#endif //CPLUS_LEARNING_MYTIME0_H
---
+++ CPP_Primer_Plus/chapter_11_useclass/usetime0.cpp
@@ -0,0 +1,35 @@
+//
+// Created by wujinxing on 2019/10/31.
+// 使用mytime类
+//
+#include <iostream>
+#include "mytime0.h"
+
+int main() {
+    using std::cout;
+    using std::endl;
+
+    Time planning;
+    Time coding(2, 40);
+    Time fixing(5, 55);
+    Time total;
+
+    cout << "planning time = ";
+    planning.Show();
+    cout << endl;
+
+    cout << "coding time = ";
+    planning.Show();
+    cout << endl;
+
+    cout << "fixing time = ";
+    fixing.Show();
+    cout << endl;
+
+    total = coding.Sum(fixing);
+    cout << "coding.Sum(fixing) = ";
+    total.Show();
+    cout << endl;
+
+    return 0;
+}
