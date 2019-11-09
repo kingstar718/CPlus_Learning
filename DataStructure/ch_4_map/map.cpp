bf149eff217d6e6f6e66b59c0d2c3e8e737a8374
kingstar718 <kingstar718@foxmail.com>
2019-11-10 00:16:41 +0800

    data structure

--- DataStructure/ch_4_map/map.cpp
+++ DataStructure/ch_4_map/map.cpp
@@ -2,6 +2,7 @@
 // Created by wujinxing on 2019/11/9.
 //
 #include <cstdlib>
+#include <cstdio>
 
 #define MaxVertexNum 100    /* 最大顶点数设为100 */
 #define INFINITY 65535  /* ∞设为双字节无符号整数的最大值65535*/
@@ -17,20 +18,74 @@ struct ENode {
 };
 typedef PtrToENode Edge;
 
+// 图结点的定义
 typedef struct GNode *PtrToGNode;
-struct GNode{
-    int Nv;
-    int Ne;
-    WeightType G[MaxVertexNum][MaxVertexNum];
-    DataType  Data[MaxVertexNum];
+struct GNode {
+    int Nv; //顶点数
+    int Ne; // 边数
+    WeightType G[MaxVertexNum][MaxVertexNum];  //邻接矩阵
+    DataType Data[MaxVertexNum];   // 存顶点的数据
+    /* 注意：很多情况下，顶点无数据，此时Data[]可以不用出现 */
 };
 
-typedef PtrToENode MGraph;  /* 以邻接矩阵存储的图类型 */
+typedef PtrToGNode MGraph;  /* 以邻接矩阵存储的图类型 */
 
-MGraph CreateGraph(int VertexNum){
-    Vertex V,W;
+MGraph CreateGraph(int VertexNum) {
+    Vertex V, W;
     MGraph Graph;
 
-    Graph = (MGraph)malloc(sizeof(struct GNode));
-    Graph->N
+    Graph = (MGraph) malloc(sizeof(struct GNode));
+    Graph->Nv = VertexNum;
+    Graph->Ne = 0;
+
+    //初始化邻接矩阵 编号从0-Nv-1
+    for (V = 0; V < Graph->Ne - 1; V++) {
+        for (W = 0; W < Graph->Nv; W++) {
+            Graph->G[V][W] = INFINITY;
+        }
+    }
+    return Graph;
+}
+
+void InsertEdge(MGraph Graph, Edge E) {
+    //插入边 <v1, v2>
+    Graph->G[E->V1][E->V2] = E->Weight;
+    Graph->G[E->V2][E->V1] = E->Weight;
+}
+
+MGraph BuildGraph() {
+    MGraph Graph;
+    Edge E;
+    Vertex V;
+    int Nv, i;
+
+    scanf("%d", &Nv); //读入顶点个数
+    Graph = CreateGraph(Nv);  // 初始化有Nv个结点但没有边的图
+    scanf("%d", &(Graph->Ne)); //读入边数
+    if (Graph->Ne != 0) { //如果有边
+        E = (Edge) malloc(sizeof(struct ENode));   // 建立边结点
+        /* 读入边，格式为"起点 终点 权重"，插入邻接矩阵 */
+        for (i = 0; i < Graph->Ne; ++i) {
+            scanf("%d %d %d", &E->V1, &E->V2, &E->Weight);
+            InsertEdge(Graph, E);/* 注意：如果权重不是整型，Weight的读入格式要改 */
+        }
+    }
+    // 如果顶点有数据的话，读入数据
+    for (V = 0; V < Graph->Nv; ++V) {
+        scanf(" %c", &(Graph->Data[V]));
+    }
+
+    for (int j = 0; j < Graph->Nv; ++j) {
+        for (int k = 0; k < Graph->Nv; ++k) {
+            printf("%d ", Graph->G[j][k]);
+        }
+        printf("\n");
+    }
+    return Graph;
+
+}
+
+int main() {
+    BuildGraph();
+    return 0;
 }
